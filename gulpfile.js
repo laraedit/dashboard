90d2faf8c2d07066b2e38b39769275d5b88f2abc
Derek Bourgeois <derek@ibourgeois.com>
2016-04-27 19:16:09 -0400

    import laravel-elixir

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir.config.assetsPath = 'assets';
+elixir.config.publicPath = 'dist';
+
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+});
